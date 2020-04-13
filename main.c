03c627f5f0cda987cb54a847c4d8aa8dcfa65aad
Aldebaranlopera14@gmail.com <Aldebaranlopera14@gmail.com>
2020-04-13 15:58:42 +0000

    my simple shell

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <shell.h>
+/**
+ *
+ *
+ *
+*/
+int main (int argc, char **argv)
+{
+	char cli[100], command[100], *parameters[20];
+	char *envp[] =
+}
---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+#endif
